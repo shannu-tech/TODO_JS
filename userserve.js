bff18b3c908ce5f7599a2836d93d90e88b36b9a5
shannu-tech <dsssrhp@gmail.com>
2026-07-07 12:43:32 +0530

    add user todo and serve

---
+++ userserve.js
@@ -0,0 +1,82 @@
+const dns = require("dns");
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
+
+const express = require("express");
+const mongoose = require("mongoose");
+const UserSchema = require("./usertodo");
+
+const app = express();
+
+app.use(express.json());
+
+const MONGO_URI =("mongodb+srv://SHANNU:opUyvSXjFSaVLNVe@nodejs.nkun5i2.mongodb.net/?appName=NODEJS");
+
+mongoose
+  .connect(MONGO_URI)
+  .then(() => console.log("✅ Database Connected Successfully"))
+  .catch((err) => console.log("❌ MongoDB Error:", err.message));
+
+app.post('/send_user',async(req,res)=>{
+    const {username} = req.body
+    const {email} = req.body
+    const {password} = req.body
+    try{
+        const newData = new UserSchema({username,email,password})
+        await newData.save()
+        return res.json({"message":"data sended"})
+    }
+    catch(err){
+        console.log(err.message)
+    }
+})
+
+app.get('/get_all_users',async(req,res)=>{
+    try{
+        const allData = await UserSchema.find()
+        return res.json(allData)
+    }
+    catch(err){
+        console.log(err.message)
+    }
+})
+app.get('/get_user/:id',async(req,res)=>{
+    try{
+        const Data=await UserSchema.findById(req.params.id);
+        return res.json(Data)
+    }
+    catch(err){
+        console.log(err.message)
+    }
+})
+app.put('/update_user/:id',async(req,res)=>{
+    const {username}=req.body
+    const {email}=req.body
+    const {password} = req.body
+    try{
+        await UserSchema.findByIdAndUpdate(req.params.id,{username,email,password})
+        return res.json({
+            message:"user was updated"
+        })
+    }
+    catch(err){
+        console.log(err.message)
+    }
+})
+
+
+app.delete("/delete/:id1/:id2",async(req,res)=>{
+    try{
+        await UserSchema.findByIdAndDelete(req.params.id1);
+        await UserSchema.findByIdAndDelete(req.params.id2);
+        return res.json({
+            message:"user deleted"
+        })
+    }
+    catch(err){
+        console.log(err.message)
+    }
+})
+
+app.listen(3000, () => {
+  console.log("🚀 Server Running on http://localhost:3000");
+});
